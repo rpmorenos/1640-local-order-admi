d8202fee5099f5d57a15f93b74b116c53d525dd8
rpmorenos <paolo6_1@hotmail.com>
2024-10-03 17:54:52 -0600

    Initialize quality-gates

---
+++ Codigo/C++/Example1.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Uso del metodo cout para imprimir un mensaje
+    cout << "Bienvenidos a" << endl;
+    cout << "Bienvenidos a" << endl;
+    cout << "Bienvenidos a" << endl;
+    cout << "la OOI!";
+
+    return 0;
+}
