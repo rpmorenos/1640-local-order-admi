8cf21d9cc5634e38cb1ed5ab663ca9e309a25942
rpmorenos <paolo6_1@hotmail.com>
2024-10-03 17:54:51 -0600

    Initial commit

---
+++ Codigo/C++/src/main/obj1/resources/code/xample/Example245.c
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Uso del metodo cout para imprimir un mensaje
+    cout << "Bienvenidos a" << endl;
+    cout << "la OOI!";
+
+    return 0;
+}
+
+void return() {
+    return 0;
+}
+
+void return() {
+    return 0;
+}
